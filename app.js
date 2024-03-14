b848b39d57a25c47de4eef67ee2accea616f68c1
MARCO MEROLA ALIBRANDI <marco.merolaalibrandi@telefonica.com>
2024-03-14 14:07:53 -0400

    adding 404 page

--- app.js
+++ app.js
@@ -27,6 +27,13 @@ app.set("view engine","ejs");
 
 app.use("/",require("./server/routes/index"));
 
+//Handle 404
+
+app.get("*", function(req,res){
+
+    res.status(404).render("404")
+})
+
 app.listen(port, ()=>{
     console.log("App listening on port ${port}");
-})
+});
